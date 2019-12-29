4643b68c61925c4accd1b5ff3914a2e6a24ec6a1
himoto <himoto@protein.osaka-u.ac.jp>
2019-12-29 15:39:09 +0900

    Check if fabs(signaling_metric[i, j, k, l] - original) < epsilon

--- biomass/analysis/nonzero_init/sensitivity.py
+++ biomass/analysis/nonzero_init/sensitivity.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 import numpy as np
-from math import log
+from math import fabs, log
 from scipy.integrate import simps
 
 from biomass.model.name2idx import parameters as C
@@ -26,9 +26,9 @@ def get_duration(time_course_vector):
     return duration
 
 
-def analyze_sensitivity(metric,nonzero_idx):
+def analyze_sensitivity(metric, nonzero_idx):
     """Compute sensitivity coefficients
-    
+
     Parameters
     ----------
     metric: str
@@ -37,93 +37,96 @@ def analyze_sensitivity(metric,nonzero_idx):
     nonzero_idx: list
         for i in nonzero_idx:
             y0[i] != 0.0
-    
+
     Returns
     -------
     sensitivity_coefficients: numpy array
     """
     sim = NumericalSimulation()
 
-    rate = 1.01 # 1% change
+    rate = 1.01  # 1% change
 
     x = f_params()
     y0 = initial_values()
-    
+
     nonzero_idx = []
-    for i,val in enumerate(y0):
+    for i, val in enumerate(y0):
         if val != 0.0:
             nonzero_idx.append(i)
     n_file = 0
     fitparam_files = os.listdir('./out')
     for file in fitparam_files:
-        if re.match(r'\d',file):
+        if re.match(r'\d', file):
             n_file += 1
 
     signaling_metric = np.full(
-        (n_file,len(nonzero_idx)+1,len(observables),len(sim.conditions)),np.nan
+        (n_file, len(nonzero_idx)+1, len(observables), len(sim.conditions)), np.nan
     )
     search_idx = search_parameter_index()
     for i in range(n_file):
-        if os.path.isfile('./out/%d/generation.npy'%(i+1)):
+        if os.path.isfile('./out/%d/generation.npy' % (i+1)):
             best_generation = \
-                np.load('./out/%d/generation.npy'%(i+1))
+                np.load('./out/%d/generation.npy' % (i+1))
             best_indiv = \
-                np.load('./out/%d/fit_param%d.npy'%(i+1,int(best_generation)))
-            for m,n in enumerate(search_idx[0]):
+                np.load('./out/%d/fit_param%d.npy' %
+                        (i+1, int(best_generation)))
+            for m, n in enumerate(search_idx[0]):
                 x[n] = best_indiv[m]
-            for m,n in enumerate(search_idx[1]):
+            for m, n in enumerate(search_idx[1]):
                 y0[n] = best_indiv[m+len(search_idx[0])]
             copy_y0 = y0[:]
-            for j,idx in enumerate(nonzero_idx):
+            for j, idx in enumerate(nonzero_idx):
                 y0[idx] = copy_y0[idx]*rate
-                if sim.simulate(x,y0) is None:
-                    for k,_ in enumerate(observables):
-                        for l,_ in enumerate(sim.conditions):
+                if sim.simulate(x, y0) is None:
+                    for k, _ in enumerate(observables):
+                        for l, _ in enumerate(sim.conditions):
                             if metric == 'duration':
-                                signaling_metric[i,j,k,l] = get_duration(
-                                    sim.simulations[k,:,l]
+                                signaling_metric[i, j, k, l] = get_duration(
+                                    sim.simulations[k, :, l]
                                 )
                             elif metric == 'integral':
-                                signaling_metric[i,j,k,l] = simps(
-                                    sim.simulations[k,:,l]
+                                signaling_metric[i, j, k, l] = simps(
+                                    sim.simulations[k, :, l]
                                 )
                             else:
                                 raise ValueError(
                                     "metric ∈ {'duration','integral'}"
                                 )
                 sys.stdout.write(
-                    '\r%d/%d'%(i*len(nonzero_idx)+j+1,n_file*len(nonzero_idx))
+                    '\r%d/%d' % (
+                        i*len(nonzero_idx)+j+1, n_file*len(nonzero_idx))
                 )
             y0 = copy_y0[:]
-            if sim.simulate(x,y0) is None:
-                for k,_ in enumerate(observables):
-                    for l,_ in enumerate(sim.conditions):
+            if sim.simulate(x, y0) is None:
+                for k, _ in enumerate(observables):
+                    for l, _ in enumerate(sim.conditions):
                         if metric == 'duration':
-                            signaling_metric[i,-1,k,l] = get_duration(
-                                sim.simulations[k,:,l]
+                            signaling_metric[i, -1, k, l] = get_duration(
+                                sim.simulations[k, :, l]
                             )
                         elif metric == 'integral':
-                            signaling_metric[i,-1,k,l] = simps(
-                                sim.simulations[k,:,l]
+                            signaling_metric[i, -1, k, l] = simps(
+                                sim.simulations[k, :, l]
                             )
                         else:
                             raise ValueError(
                                 "metric ∈ {'duration','integral'}"
                             )
     sensitivity_coefficients = np.empty(
-        (n_file,len(nonzero_idx),len(observables),len(sim.conditions))
+        (n_file, len(nonzero_idx), len(observables), len(sim.conditions))
     )
     for i in range(n_file):
-        for j,_ in enumerate(nonzero_idx):
-            for k,_ in enumerate(observables):
-                for l,_ in enumerate(sim.conditions):
-                    if np.isnan(signaling_metric[i,j,k,l]):
-                        sensitivity_coefficients[i,j,k,l] = np.nan
-                    elif signaling_metric[i,-1,k,l] < sys.float_info.epsilon or \
-                            (signaling_metric[i,j,k,l]/signaling_metric[i,-1,k,l]) < 0:
-                        sensitivity_coefficients[i,j,k,l] = 0.0
+        for j, _ in enumerate(nonzero_idx):
+            for k, _ in enumerate(observables):
+                for l, _ in enumerate(sim.conditions):
+                    if np.isnan(signaling_metric[i, j, k, l]):
+                        sensitivity_coefficients[i, j, k, l] = np.nan
+                    elif fabs(signaling_metric[i, j, k, l] - signaling_metric[i, -1, k, l]) < sys.float_info.epsilon or \
+                            (signaling_metric[i, j, k, l]/signaling_metric[i, -1, k, l]) < 0:
+                        sensitivity_coefficients[i, j, k, l] = 0.0
                     else:
-                        sensitivity_coefficients[i,j,k,l] = (
-                            log(signaling_metric[i,j,k,l]/signaling_metric[i,-1,k,l])/log(rate)
+                        sensitivity_coefficients[i, j, k, l] = (
+                            log(signaling_metric[i, j, k, l] /
+                                signaling_metric[i, -1, k, l])/log(rate)
                         )
     return sensitivity_coefficients
--- biomass/analysis/reaction/sensitivity.py
+++ biomass/analysis/reaction/sensitivity.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 import numpy as np
-from math import log
+from math import fabs, log
 from scipy.integrate import simps
 
 from biomass.model.name2idx import parameters as C
@@ -27,9 +27,9 @@ def get_duration(time_course_vector):
     return duration
 
 
-def analyze_sensitivity(metric,num_reaction):
+def analyze_sensitivity(metric, num_reaction):
     """Compute sensitivity coefficients
-    
+
     Parameters
     ----------
     metric: str
@@ -37,14 +37,14 @@ def analyze_sensitivity(metric,num_reaction):
         - 'integral': The integral of concentration over the observation time.
     num_reaction: int
         len(v) in model/differential_equation.py
-    
+
     Returns
     -------
     sensitivity_coefficients: numpy array
     """
     sim = NumericalSimulation()
 
-    rate = 1.01 # 1% change
+    rate = 1.01  # 1% change
 
     x = f_params()
     y0 = initial_values()
@@ -52,56 +52,58 @@ def analyze_sensitivity(metric,num_reaction):
     n_file = 0
     fitparam_files = os.listdir('./out')
     for file in fitparam_files:
-        if re.match(r'\d',file):
+        if re.match(r'\d', file):
             n_file += 1
 
     signaling_metric = np.full(
-        (n_file,num_reaction,len(observables),len(sim.conditions)),np.nan
+        (n_file, num_reaction, len(observables), len(sim.conditions)), np.nan
     )
     search_idx = search_parameter_index()
     for i in range(n_file):
-        if os.path.isfile('./out/%d/generation.npy'%(i+1)):
+        if os.path.isfile('./out/%d/generation.npy' % (i+1)):
             best_generation = \
-                np.load('./out/%d/generation.npy'%(i+1))
+                np.load('./out/%d/generation.npy' % (i+1))
             best_indiv = \
-                np.load('./out/%d/fit_param%d.npy'%(i+1,int(best_generation)))
-            for m,n in enumerate(search_idx[0]):
+                np.load('./out/%d/fit_param%d.npy' %
+                        (i+1, int(best_generation)))
+            for m, n in enumerate(search_idx[0]):
                 x[n] = best_indiv[m]
-            for m,n in enumerate(search_idx[1]):
+            for m, n in enumerate(search_idx[1]):
                 y0[n] = best_indiv[m+len(search_idx[0])]
             for j in range(num_reaction):
                 ode.perturbation = [1]*num_reaction
                 ode.perturbation[j] = rate
-                if sim.simulate(x,y0) is None:
-                    for k,_ in enumerate(observables):
-                        for l,_ in enumerate(sim.conditions):
+                if sim.simulate(x, y0) is None:
+                    for k, _ in enumerate(observables):
+                        for l, _ in enumerate(sim.conditions):
                             if metric == 'duration':
-                                signaling_metric[i,j,k,l] = get_duration(
-                                    sim.simulations[k,:,l]
+                                signaling_metric[i, j, k, l] = get_duration(
+                                    sim.simulations[k, :, l]
                                 )
                             elif metric == 'integral':
-                                signaling_metric[i,j,k,l] = simps(
-                                    sim.simulations[k,:,l]
+                                signaling_metric[i, j, k, l] = simps(
+                                    sim.simulations[k, :, l]
                                 )
                             else:
                                 raise ValueError(
                                     "metric ∈ {'duration','integral'}"
                                 )
                 sys.stdout.write(
-                    '\r%d/%d'%(i*num_reaction+j+1,n_file*num_reaction)
+                    '\r%d/%d' % (i*num_reaction+j+1, n_file*num_reaction)
                 )
     sensitivity_coefficients = np.empty_like(signaling_metric)
     for i in range(n_file):
         for j in range(num_reaction):
-            for k,_ in enumerate(observables):
-                for l,_ in enumerate(sim.conditions):
-                    if np.isnan(signaling_metric[i,j,k,l]):
-                        sensitivity_coefficients[i,j,k,l] = np.nan
-                    elif signaling_metric[i,0,k,l] < sys.float_info.epsilon or \
-                            (signaling_metric[i,j,k,l]/signaling_metric[i,0,k,l]) < 0:
-                        sensitivity_coefficients[i,j,k,l] = 0.0
+            for k, _ in enumerate(observables):
+                for l, _ in enumerate(sim.conditions):
+                    if np.isnan(signaling_metric[i, j, k, l]):
+                        sensitivity_coefficients[i, j, k, l] = np.nan
+                    elif fabs(signaling_metric[i, j, k, l] - signaling_metric[i, 0, k, l]) < sys.float_info.epsilon or \
+                            (signaling_metric[i, j, k, l]/signaling_metric[i, 0, k, l]) < 0:
+                        sensitivity_coefficients[i, j, k, l] = 0.0
                     else:
-                        sensitivity_coefficients[i,j,k,l] = (
-                            log(signaling_metric[i,j,k,l]/signaling_metric[i,0,k,l])/log(rate)
+                        sensitivity_coefficients[i, j, k, l] = (
+                            log(signaling_metric[i, j, k, l] /
+                                signaling_metric[i, 0, k, l])/log(rate)
                         )
     return sensitivity_coefficients
