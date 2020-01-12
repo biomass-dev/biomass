66ab98a80cc149af1a1931aae5b0274738e3d14c
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-12 15:50:50 +0900

    Don't use '\'

--- biomass/param_estim/dynamics.py
+++ biomass/param_estim/dynamics.py
@@ -7,9 +7,10 @@ from biomass.observable import observables, NumericalSimulation
 from biomass.param_estim import plot_func
 from biomass.param_estim.search_parameter import search_parameter_index
 
-def simulate_all(viz_type,show_all,stdev):
+
+def simulate_all(viz_type, show_all, stdev):
     """Simulate ODE model with estimated parameter values.
-    
+
     Parameters
     ----------
     viz_type : str
@@ -23,7 +24,7 @@ def simulate_all(viz_type,show_all,stdev):
         If True, the standard deviation of simulated values will be shown
         (only when viz_type == 'average')
     """
-    if not viz_type in ['best','average','original']:
+    if not viz_type in ['best', 'average', 'original']:
         try:
             int(viz_type)
         except ValueError:
@@ -38,96 +39,101 @@ def simulate_all(viz_type,show_all,stdev):
         if os.path.isdir('./out'):
             fit_param_files = os.listdir('./out')
             for file in fit_param_files:
-                if re.match(r'\d',file):
+                if re.match(r'\d', file):
                     n_file += 1
-
-    simulations_all = np.full((len(observables),n_file,len(sim.t),len(sim.conditions)),np.nan)
-    
+    simulations_all = np.full(
+        (len(observables), n_file, len(sim.t), len(sim.conditions)), np.nan
+    )
     if n_file > 0:
         if n_file == 1 and viz_type == 'average':
             viz_type = 'best'
         for i in range(n_file):
-            (sim,successful) = validate(i+1,x,y0)
+            (sim, successful) = validate(i+1, x, y0)
             if successful:
-                for j,_ in enumerate(observables):
-                    simulations_all[j,i,:,:] = sim.simulations[j,:,:]
+                for j, _ in enumerate(observables):
+                    simulations_all[j, i, :, :] = sim.simulations[j, :, :]
 
         best_fitness_all = np.empty(n_file)
         for i in range(n_file):
-            if os.path.isfile('./out/%d/best_fitness.npy'%(i+1)):
-                best_fitness_all[i] = np.load('./out/%d/best_fitness.npy'%(i+1))
+            if os.path.isfile('./out/%d/best_fitness.npy' % (i+1)):
+                best_fitness_all[i] = np.load(
+                    './out/%d/best_fitness.npy' % (i+1)
+                )
             else:
                 best_fitness_all[i] = np.inf
 
         best_paramset = np.argmin(best_fitness_all) + 1
-        write_best_fit_param(best_paramset,x,y0)
+        write_best_fit_param(best_paramset, x, y0)
 
         if viz_type == 'average':
             pass
         elif viz_type == 'best':
-            sim = validate(int(best_paramset),x,y0)[0]
+            sim = validate(int(best_paramset), x, y0)[0]
         elif int(viz_type) <= n_file:
-            sim = validate(int(viz_type),x,y0)[0]
+            sim = validate(int(viz_type), x, y0)[0]
         else:
             raise ValueError(
-                '%d is larger than n_fit_param(%d)'%(int(viz_type),n_file)
+                '%d is larger than n_fit_param(%d)' % (int(viz_type), n_file)
             )
         if n_file >= 2:
-            save_param_range(n_file,x,y0,portrait=True)
+            save_param_range(n_file, x, y0, portrait=True)
     else:
-        if sim.simulate(x,y0) is not None:
+        if sim.simulate(x, y0) is not None:
             print('Simulation failed.')
 
-    plot_func.timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all)
+    plot_func.timecourse(
+        sim, n_file, viz_type, show_all, stdev, simulations_all
+    )
 
 
-def update_param(paramset,x,y0):
+def update_param(paramset, x, y0):
     search_idx = search_parameter_index()
 
-    if os.path.isfile('./out/%d/generation.npy'%(paramset)):
-        best_generation = \
-            np.load('./out/%d/generation.npy'%(paramset))
-        best_indiv = \
-            np.load('./out/%d/fit_param%d.npy'%(paramset,int(best_generation)))
-
-        for i,j in enumerate(search_idx[0]):
+    if os.path.isfile('./out/%d/generation.npy' % (paramset)):
+        best_generation = np.load(
+            './out/%d/generation.npy' % (paramset)
+        )
+        best_indiv = np.load(
+            './out/%d/fit_param%d.npy' % (paramset, int(best_generation))
+        )
+        for i, j in enumerate(search_idx[0]):
             x[j] = best_indiv[i]
-        for i,j in enumerate(search_idx[1]):
+        for i, j in enumerate(search_idx[1]):
             y0[j] = best_indiv[i+len(search_idx[0])]
     else:
         pass
     return x, y0
 
 
-def validate(nth_paramset,x,y0):
+def validate(nth_paramset, x, y0):
     # -------------------------------------------------------------------------
     # Validates the dynamical viability of a set of estimated parameter values.
     # -------------------------------------------------------------------------
     sim = NumericalSimulation()
-    
-    (x,y0) = update_param(nth_paramset,x,y0)
 
-    if sim.simulate(x,y0) is None:
-        return sim,True
+    (x, y0) = update_param(nth_paramset, x, y0)
+
+    if sim.simulate(x, y0) is None:
+        return sim, True
     else:
-        print('Simulation failed.\nparameter_set #%d'%(nth_paramset))
-        return sim,False
+        print('Simulation failed.\nparameter_set #%d' % (nth_paramset))
+        return sim, False
 
 
-def write_best_fit_param(best_paramset,x,y0):
+def write_best_fit_param(best_paramset, x, y0):
 
-    (x,y0) = update_param(best_paramset,x,y0)
-    
+    (x, y0) = update_param(best_paramset, x, y0)
+
     with open('./out/best_fit_param.txt', mode='w') as f:
         f.write(
-            '# param set: %d\n'%(best_paramset)
+            '# param set: %d\n' % (best_paramset)
         )
         f.write(
             '\n### Param. const\n'
         )
         for i in range(model.C.len_f_params):
             f.write(
-                'x[C.%s] = %8.3e\n'%(model.C.param_names[i],x[i])
+                'x[C.%s] = %8.3e\n' % (model.C.param_names[i], x[i])
             )
         f.write(
             '\n### Non-zero initial conditions\n'
@@ -135,26 +141,30 @@ def write_best_fit_param(best_paramset,x,y0):
         for i in range(model.V.len_f_vars):
             if y0[i] != 0:
                 f.write(
-                    'y0[V.%s] = %8.3e\n'%(model.V.var_names[i],y0[i])
+                    'y0[V.%s] = %8.3e\n' % (model.V.var_names[i], y0[i])
                 )
 
 
-def save_param_range(n_file,x,y0,portrait):
+def save_param_range(n_file, x, y0, portrait):
     search_idx = search_parameter_index()
-    search_param_matrix = np.empty((n_file,len(search_idx[0])+len(search_idx[1])))
-
-    for nth_paramset in range(1,n_file+1):
-        if os.path.isfile('./out/%d/generation.npy'%(nth_paramset)):
-            best_generation = \
-                np.load('./out/%d/generation.npy'%(nth_paramset))
-            best_indiv = \
-                np.load('./out/%d/fit_param%d.npy'%(nth_paramset,int(best_generation)))
+    search_param_matrix = np.empty(
+        (n_file, len(search_idx[0]) + len(search_idx[1]))
+    )
+    for nth_paramset in range(1, n_file+1):
+        if os.path.isfile('./out/%d/generation.npy' % (nth_paramset)):
+            best_generation = np.load(
+                './out/%d/generation.npy' % (nth_paramset)
+            )
+            best_indiv = np.load(
+                './out/%d/fit_param%d.npy' % (
+                    nth_paramset, int(best_generation))
+            )
         else:
             best_indiv = np.empty(len(search_idx[0])+len(search_idx[1]))
-            for i,j in enumerate(search_idx[0]):
+            for i, j in enumerate(search_idx[0]):
                 best_indiv[i] = x[j]
-            for i,j in enumerate(search_idx[1]):
+            for i, j in enumerate(search_idx[1]):
                 best_indiv[i+len(search_idx[0])] = y0[j]
 
-        search_param_matrix[nth_paramset-1,:] = best_indiv
-    plot_func.param_range(search_idx,search_param_matrix,portrait)
+        search_param_matrix[nth_paramset-1, :] = best_indiv
+    plot_func.param_range(search_idx, search_param_matrix, portrait)
