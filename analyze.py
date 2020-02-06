21caa7ace7706d76a9c192691edf2d32baad8620
johannesnicolaus <johannes.nicolaus@gmail.com>
2020-02-06 16:10:35 +0900

    Add argument support

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import os
 import warnings
+import sys
 warnings.filterwarnings('ignore')
 from biomass.analysis import reaction, nonzero_init
 
@@ -7,7 +8,20 @@ if not os.path.isdir('./figure'):
     os.mkdir('./figure')
     
 if __name__ == '__main__':
-    reaction.sensitivity_barplot(metric='integral')
+    args = sys.argv
+    if len(args) > 2:
+        print(
+                "Too many arguments! Try one of these: 'integral', 'amplitude', 'duration'"
+            )
+        sys.exit()
+    else:
+        if str(args[1]) not in ['integral', 'amplitude', 'duration']:
+            print(
+                "Available arguments are: 'integral', 'amplitude', 'duration'"
+            )
+            sys.exit()
+
+    reaction.sensitivity_barplot(metric=str(args[1]))
 #   reaction.sensitivity_heatmap(metric='integral')
-#   nonzero_init.sensitivity_barplot(metric='integral')
+#    nonzero_init.sensitivity_barplot(metric=str(args[1]))
 #   nonzero_init.sensitivity_heatmap(metric='integral')
