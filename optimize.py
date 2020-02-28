958aa9a040ed2e8c56d291cb0201bce4332a74a4
himoto <himoto@protein.osaka-u.ac.jp>
2020-02-28 11:00:36 +0900

    Import optimize from biomass.param_estim

--- optimize.py
+++ optimize.py
@@ -6,22 +6,30 @@ import multiprocessing
 import warnings
 warnings.filterwarnings('ignore')
 
-from biomass import optimize
+from biomass.param_estim import optimize
 
 
 def run_ga(nth_paramset):
     if not os.path.isdir('./out'):
         os.mkdir('./out')
     try:
-        files = os.listdir('./out/%d'%(nth_paramset))
+        files = os.listdir(
+            './out/{}'.format(nth_paramset)
+        )
         for file in files:
-            if any(map(file.__contains__,('.npy','log'))):
-                os.remove('./out/%d/%s'%(nth_paramset,file))
+            if any(map(file.__contains__, ('.npy', '.log'))):
+                os.remove(
+                    './out/%d/%s' % (
+                        nth_paramset, file
+                    )
+                )
     except FileNotFoundError:
-        os.mkdir('./out/%d'%(nth_paramset))
+        os.mkdir(
+            './out/{}'.format(nth_paramset)
+        )
 
     optimize(nth_paramset)
-    
+
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 2:
@@ -29,5 +37,5 @@ if __name__ == '__main__':
     elif len(args) == 3:
         n_proc = max(1, multiprocessing.cpu_count() - 1)
         p = multiprocessing.Pool(processes=n_proc)
-        p.map(run_ga, range(int(args[1]),int(args[2])+1))
+        p.map(run_ga, range(int(args[1]), int(args[2]) + 1))
         p.close()
--- optimize_continue.py
+++ optimize_continue.py
@@ -5,15 +5,17 @@ import multiprocessing
 import warnings
 warnings.filterwarnings('ignore')
 
-from biomass import optimize,optimize_continue
+from biomass.param_estim import optimize, optimize_continue
 
 def run_ga_continue(nth_paramset):
-    if not os.path.isdir('./out/%d'%(nth_paramset)):
-        os.mkdir('./out/%d'%(nth_paramset))
+    if not os.path.isdir('./out/{}'.format(nth_paramset)):
+        os.mkdir(
+            './out/{}'.format(nth_paramset)
+        )
         optimize(nth_paramset)
     else:
         optimize_continue(nth_paramset)
-        
+
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 2:
@@ -21,5 +23,5 @@ if __name__ == '__main__':
     elif len(args) == 3:
         n_proc = max(1, multiprocessing.cpu_count() - 1)
         p = multiprocessing.Pool(processes=n_proc)
-        p.map(run_ga_continue, range(int(args[1]),int(args[2])+1))
+        p.map(run_ga_continue, range(int(args[1]), int(args[2]) + 1))
         p.close()
