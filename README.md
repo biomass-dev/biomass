40445ace6c31faf62aad3e67267921d730431c63
himoto <himoto@protein.osaka-u.ac.jp>
2020-03-06 17:32:34 +0900

    Move description of ga_v2 to ga/ga_init

--- README.md
+++ README.md
@@ -116,43 +116,6 @@ $ python analyze.py integral
 
 Control coefficients for integrated pc-Fos are shown by bars (blue, EGF; red, HRG). Numbers above bars indicate the reaction indices, and error bars correspond to simulation standard deviation.
 
-## Optimization algorithm
-[`biomass/param_estim/ga`](biomass/param_estim/ga)
-#### ga_v1:
-Parameter values are searched by genetic algorithm with Unimodal Normal Distribution Crossover (UNDX) and Minimal Generation Gap (MGG).
-
-#### ga_v2:
-ga_v2 optimizes an objective function through the following procedure.
-
-1. **Initialization**<br>
-As an initial population, create *n*<sub>*p*</sub> individuals randomly. ga_v2 also represents individuals as *n*-dimensional real number vectors, where *n* is the dimension of the search space. Set Generation to 0, and set the iteration number of converging operations *N<sub>iter</sub>* to 1.
-
-1. **Selection for reproduction**<br>
-As parents for the recombination operator, ENDX, select *m* individuals, **p**<sub>1</sub>, **p**<sub>2</sub>, · · · ,**p**<sub>*m*</sub>, without replacement from the population.
-
-1. **Generation of offsprings**<br>
-Generate *N<sub>*c*</sub>* children by applying ENDX to the selected parents. This algorithm assigns the worst objective value to the children.
-
-1. **Local Search (NDM/MGG)**<br>
-Apply the local search method to the best individual in a family consisting of the two parents, i.e., **p**<sub>1</sub> and **p**<sub>2</sub>, and their children. Note here that the children are assumed to have the worst objective value. Thus, whenever the objective values of the two parents have been actually computed in previous generations, the algorithm applies the local search to either of the parents. When all of the individuals in the family have the same objective value, on the other hand, the local search is applied to a randomly selected individual from the family.
-
-1. **Selection for survival**<br>
-Select two individuals from the family. The first selected individual should be the individual with the best objective value, and the second should be selected randomly. Then, replace the two parents (**p**<sub>1</sub> and **p**<sub>2</sub>) with the selected individuals. Note that the individual to which the local search has been applied in the previous step is always selected as the best.
-
-1. **Application of ENDX/MGG**<br>
-To achieve a good search performance, ga_v2 optimizes a function, gradually narrowing the search space. For this purpose, the converging phase slightly converges the population by repeating the following procedure *N<sub>iter</sub>* times.
-    1. Select *m* individuals without replacement from the population. The selected individuals, expressed here as **p**<sub>1</sub>, **p**<sub>2</sub>, · · · , **p**<sub>*m*</sub>, are used as the parents for an extended normal distribution crossover (ENDX) applied in the next step.
-
-    1. Generate *N<sub>*c*</sub>* children by applying ENDX to the parents selected in the previous step. To reduce the computational cost, ga_v2 forgoes any computation of the objective values of the *N<sub>*c*</sub>* individuals generated here. Instead, the algorithm assigns the newly generated children a single objective value, one which is inferior to the objective values of any of the possible candidate solutions.
-
-    1. Select two individuals from a family containing the two parents, i.e., **p**<sub>1</sub> and **p**<sub>2</sub>, and their children. The first selected individual should be the one with the best objective value, and the second should be selected randomly. Then, replace the two parents with the selected individuals.
-
-1. **Adaptation of *N<sub>iter</sub>***<br>
-If the best individual has not improved during the last *n*<sub>*p*</sub> generations, *N<sub>iter</sub>* ← 2 × *N<sub>iter</sub>*. Otherwise, set *N<sub>iter</sub>* to 1.
-
-1. **Termination**<br>
-Stop if the halting criteria are satisfied. Otherwise, *Generation* ← *Generation* +1, and return to the step 2.
-
 ## Installation
     $ git clone https://github.com/okadalabipr/biomass.git
 
--- biomass/param_estim/ga/ga_init.py
+++ biomass/param_estim/ga/ga_init.py
@@ -41,9 +41,9 @@ def ga_v1(nth_paramset, max_generation, n_population, n_children, n_gene,
     population = get_initial_population(
         nth_paramset, n_population, n_gene, search_idx, search_region
     )
-    with open('./out/%d/out.log' % (nth_paramset), mode='w') as f:
+    with open('./out/{:d}/out.log'.format(nth_paramset), mode='w') as f:
         f.write(
-            'Generation1: Best Fitness = %e\n' % (
+            'Generation1: Best Fitness = {:e}\n'.format(
                 population[0, -1]
             )
         )
@@ -53,17 +53,17 @@ def ga_v1(nth_paramset, max_generation, n_population, n_children, n_gene,
     best_fitness = population[0, -1]
 
     np.save(
-        './out/%d/generation.npy' % (
+        './out/{:d}/generation.npy'.format(
             nth_paramset
         ), 1
     )
     np.save(
-        './out/%d/fit_param1' % (
+        './out/{:d}/fit_param1'.format(
             nth_paramset
         ), best_indiv
     )
     np.save(
-        './out/%d/best_fitness.npy' % (
+        './out/{:d}/best_fitness.npy'.format(
             nth_paramset
         ), best_fitness
     )
@@ -83,29 +83,29 @@ def ga_v1(nth_paramset, max_generation, n_population, n_children, n_gene,
         )
         if population[0, -1] < best_fitness:
             np.save(
-                './out/%d/generation.npy' % (
+                './out/{:d}/generation.npy'.format(
                     nth_paramset
                 ), generation + 1
             )
             np.save(
-                './out/%d/fit_param%d.npy' % (
+                './out/{:d}/fit_param{:d}.npy'.format(
                     nth_paramset, generation + 1
                 ), best_indiv
             )
         best_fitness = population[0, -1]
         np.save(
-            './out/%d/best_fitness.npy' % (
+            './out/{:d}/best_fitness.npy'.format(
                 nth_paramset
             ), best_fitness
         )
         np.save(
-            './out/%d/count_num.npy' % (
+            './out/{:d}/count_num.npy'.format(
                 nth_paramset
             ), generation + 1
         )
-        with open('./out/%d/out.log' % (nth_paramset), mode='a') as f:
+        with open('./out/{:d}/out.log'.format(nth_paramset), mode='a') as f:
             f.write(
-                'Generation%d: Best Fitness = %e\n' % (
+                'Generation{:d}: Best Fitness = {:e}\n'.format(
                     generation + 1, best_fitness
                 )
             )
@@ -128,9 +128,73 @@ def ga_v1(nth_paramset, max_generation, n_population, n_children, n_gene,
 
 def ga_v2(nth_paramset, max_generation, n_population, n_children, n_gene,
           allowable_error, search_idx, search_region):
+    """ga_v2 optimizes an objective function through the following procedure.
+
+    1. Initialization
+        As an initial population, create np individuals randomly. 
+        ga_v2 also represents individuals as n-dimensional real number vectors, 
+        where n is the dimension of the search space. Set Generation to 0, and 
+        set the iteration number of converging operations Niter to 1.
+
+    2. Selection for reproduction
+        As parents for the recombination operator, ENDX, select m individuals, 
+        p1, p2, · · · ,pm, without replacement from the population.
+
+    3. Generation of offsprings
+        Generate Nc children by applying ENDX to the selected parents. 
+        This algorithm assigns the worst objective value to the children.
+
+    4. Local Search (NDM/MGG)
+        Apply the local search method to the best individual in a family 
+        consisting of the two parents, i.e., p1 and p2, and their children. 
+        Note here that the children are assumed to have the worst objective value. 
+        Thus, whenever the objective values of the two parents have been actually 
+        computed in previous generations, the algorithm applies the local search 
+        to either of the parents. When all of the individuals in the family have 
+        the same objective value, on the other hand, the local search is applied 
+        to a randomly selected individual from the family.
+
+    5. Selection for survival
+        Select two individuals from the family. The first selected individual 
+        should be the individual with the best objective value, and the second 
+        should be selected randomly. Then, replace the two parents (p1 and p2) 
+        with the selected individuals. Note that the individual to which the 
+        local search has been applied in the previous step is always selected 
+        as the best.
+
+    6. Application of ENDX/MGG
+        To achieve a good search performance, ga_v2 optimizes a function, 
+        gradually narrowing the search space. For this purpose, the converging phase 
+        slightly converges the population by repeating the following procedure Niter times.
+
+        (i) Select m individuals without replacement from the population. 
+            The selected individuals, expressed here as p1, p2, · · · , pm, are 
+            used as the parents for an extended normal distribution crossover 
+            (ENDX) applied in the next step.
+
+        (ii) Generate Nc children by applying ENDX to the parents selected in 
+            the previous step. To reduce the computational cost, ga_v2 forgoes 
+            any computation of the objective values of the Nc individuals 
+            generated here. Instead, the algorithm assigns the newly generated 
+            children a single objective value, one which is inferior to the 
+            objective values of any of the possible candidate solutions.
+
+        (iii) Select two individuals from a family containing the two parents, 
+            i.e., p1 and p2, and their children. The first selected individual 
+            should be the one with the best objective value, and the second 
+            should be selected randomly.Then, replace the two parents with 
+            the selected individuals.
+    7. Adaptation of Niter
+        If the best individual has not improved during the last np generations, 
+        Niter ← 2 × Niter. Otherwise, set Niter to 1.
+
+    8. Termination
+        Stop if the halting criteria are satisfied.
+        Otherwise, Generation ← Generation +1, and return to the step 2.
+    """
     if n_population < n_gene + 2:
         raise ValueError(
-            'n_population must be larger than %d' % (
+            'n_population must be larger than {:d}'.format(
                 n_gene + 2
             )
         )
@@ -142,9 +206,9 @@ def ga_v2(nth_paramset, max_generation, n_population, n_children, n_gene,
     )
     n0[0] = population[0, -1]
 
-    with open('./out/%d/out.log' % (nth_paramset), mode='w') as f:
+    with open('./out/{:d}/out.log'.format(nth_paramset), mode='w') as f:
         f.write(
-            'Generation1: Best Fitness = %e\n' % (
+            'Generation1: Best Fitness = {:e}\n'.format(
                 population[0, -1]
             )
         )
@@ -154,17 +218,17 @@ def ga_v2(nth_paramset, max_generation, n_population, n_children, n_gene,
     best_fitness = population[0, -1]
 
     np.save(
-        './out/%d/generation.npy' % (
+        './out/{:d}/generation.npy'.format(
             nth_paramset
         ), 1
     )
     np.save(
-        './out/%d/fit_param1.npy' % (
+        './out/{:d}/fit_param1.npy'.format(
             nth_paramset
         ), best_indiv
     )
     np.save(
-        './out/%d/best_fitness.npy' % (
+        './out/{:d}/best_fitness.npy'.format(
             nth_paramset
         ), best_fitness
     )
@@ -206,29 +270,29 @@ def ga_v2(nth_paramset, max_generation, n_population, n_children, n_gene,
         )
         if population[0, -1] < best_fitness:
             np.save(
-                './out/%d/generation.npy' % (
+                './out/{:d}/generation.npy'.format(
                     nth_paramset
                 ), generation + 1
             )
             np.save(
-                './out/%d/fit_param%d.npy' % (
+                './out/{:d}/fit_param{:d}.npy'.format(
                     nth_paramset, generation + 1
                 ), best_indiv
             )
         best_fitness = population[0, -1]
         np.save(
-            './out/%d/best_fitness.npy' % (
+            './out/{:d}/best_fitness.npy'.format(
                 nth_paramset
             ), best_fitness
         )
         np.save(
-            './out/%d/count_num.npy' % (
+            './out/{:d}/count_num.npy'.format(
                 nth_paramset
             ), generation + 1
         )
-        with open('./out/%d/out.log' % (nth_paramset), mode='a') as f:
+        with open('./out/{:d}/out.log'.format(nth_paramset), mode='a') as f:
             f.write(
-                'Generation%d: Best Fitness = %e\n' % (
+                'Generation{:d}: Best Fitness = {:e}\n'.format(
                     generation + 1, best_fitness
                 )
             )
@@ -252,7 +316,7 @@ def ga_v2(nth_paramset, max_generation, n_population, n_children, n_gene,
 def get_initial_population(nth_paramset, n_population, n_gene, search_idx, search_region):
     population = np.full((n_population, n_gene+1), np.inf)
 
-    with open('./out/%d/initpop.log' % (nth_paramset), mode='w') as f:
+    with open('./out/{:d}/initpop.log'.format(nth_paramset), mode='w') as f:
         f.write(
             'Generating the initial population. . .\n'
         )
@@ -262,9 +326,9 @@ def get_initial_population(nth_paramset, n_population, n_gene, search_idx, searc
             population[i, -1] = objective(
                 population[i, :n_gene], search_idx, search_region
             )
-        with open('./out/%d/initpop.log' % (nth_paramset), mode='a') as f:
+        with open('./out/{:d}/initpop.log'.format(nth_paramset), mode='a') as f:
             f.write(
-                '%d/%d\n' % (
+                '{:d}/{:d}\n'.format(
                     i + 1, n_population
                 )
             )
