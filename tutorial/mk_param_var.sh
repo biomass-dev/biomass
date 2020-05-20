316d63002f08e723cc3f3eb0d4926a4456f04bb8
Johannes Nicolaus <johannes.nicolaus@gmail.com>
2020-05-20 13:57:11 +0900

    update mk_param_var.sh

--- tutorial/mk_param_var.sh
+++ tutorial/mk_param_var.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# script to extract parameters from param_const.py
-grep '\[C' ../biomass/model/param_const.py | \
+# script to extract parameters from set_model.py
+# extract lines between line containing def f_params and def initial_values
+sed -n '/def f_params/,/def initial_values/p' ../biomass/model/set_model.py | \
+grep '\[C' | \
 sed 's/x\[C\.//g' | \
 sed 's/\].*//g' | \
 awk '!seen[$0]++' | \
@@ -11,13 +13,18 @@ sed 's/$/,/' |
 sed -e 's/^/	/g' |
 sed -e 's/    //g' > param_var_mid
 
-echo 'param_names = [\' > first_line
+echo 'param_names = [' > first_line
 
-echo "	##
-	'len_f_params'\
-]
-for idx,name in enumerate(param_names):
-    exec('%s=%d'%(name,idx))" > last_line
+echo "]	
+
+for idx, name in enumerate(param_names):
+    exec(
+        '{} = {:d}'.format(
+            name, idx
+        )
+    )
+
+len_f_params = len(param_names)" > last_line
 
 
 cat first_line param_var_mid last_line > ../biomass/model/name2idx/parameters.py
@@ -26,10 +33,11 @@ rm first_line
 rm param_var_mid
 rm last_line
 
+
 echo "done processing parameters"
 
 # script to extract variables from differential_equation.py
-grep 'dydt\[V' ../biomass/model/differential_equation.py | \
+grep 'dydt\[V' ../biomass/model/set_model.py | \
 sed 's/dydt\[V\.//g' | \
 sed 's/\].*//g' | \
 awk '!seen[$0]++' | \
@@ -39,13 +47,18 @@ sed 's/$/,/' |
 sed -e 's/^/	/g' |
 sed -e 's/    //g' > var_var_mid
 
-echo 'var_names = [\' > first_line
+echo 'var_names = [' > first_line
 
-echo "	##
-	'len_f_vars'\
-]
-for idx,name in enumerate(var_names):
-    exec('%s=%d'%(name,idx))" > last_line
+echo "]
+
+for idx, name in enumerate(var_names):
+    exec(
+        '{} = {:d}'.format(
+            name, idx
+        )
+    )
+
+len_f_vars = len(var_names)" > last_line
 
 
 cat first_line var_var_mid last_line > ../biomass/model/name2idx/variables.py
