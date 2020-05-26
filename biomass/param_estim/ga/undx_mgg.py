3c8d30f651fd989098ead2fae3b483fb78b86984
himoto <31299606+himoto@users.noreply.github.com>
2020-05-26 18:35:24 +0900

    Don't use p1, p2, p3

--- biomass/param_estim/ga/undx_mgg.py
+++ biomass/param_estim/ga/undx_mgg.py
@@ -10,14 +10,25 @@ def _undx(parents, n_gene):
     ALPHA = 0.5
     BETA = 0.35 / (n_gene**0.5)
 
-    p1 = parents[0, :n_gene]
-    p2 = parents[1, :n_gene]
-    p3 = parents[2, :n_gene]
-    d1 = np.linalg.norm(p2-p1)
+    d1 = np.linalg.norm(parents[1, :n_gene] - parents[0, :n_gene])
     d2 = np.linalg.norm(
-        (p3-p1) - (np.dot((p3-p1), (p2-p1))/(d1**2)) * (p2-p1)
+        (
+            parents[2, :n_gene] - parents[0, :n_gene]
+        ) - (
+            np.dot(
+                (
+                    parents[2, :n_gene]-parents[0, :n_gene]
+                ), (
+                    parents[1, :n_gene]-parents[0, :n_gene]
+                )
+            ) / (
+                d1 ** 2
+            )
+        ) * (
+            parents[1, :n_gene]-parents[0, :n_gene]
+        )
     )
-    e1 = p1/d1
+    e1 = parents[0, :n_gene]/d1
 
     t = np.random.normal(scale=BETA, size=n_gene) * d2
     t = t - np.dot(t, e1) * e1
