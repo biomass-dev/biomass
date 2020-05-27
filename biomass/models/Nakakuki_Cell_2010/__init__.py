d98b603134ae0d1854f8cef888c7cccd51c9878c
himoto <31299606+himoto@users.noreply.github.com>
2020-05-27 11:30:45 +0900

    Add Nakakuki_Cell_2010

---
+++ biomass/models/Nakakuki_Cell_2010/__init__.py
@@ -0,0 +1,6 @@
+from .name2idx import C, V
+from .set_model import *
+from .observable import *
+from .fitness import decode_gene2variable, objective
+from .set_search_param import get_search_index, get_search_region, update_param
+from .reaction_network import rxn2proc
