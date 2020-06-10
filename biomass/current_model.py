da7b20a9a6877b086ebd3c6923a5c82c1cc1b740
himoto <31299606+himoto@users.noreply.github.com>
2020-06-10 17:33:04 +0900

    Update current_model

--- biomass/current_model.py
+++ biomass/current_model.py
@@ -5,100 +5,128 @@ import numpy as np
 
 from biomass.models.Nakakuki_Cell_2010 import *
 
+class CurrentModel(object):
+    def __init__(self,
+                 parameters=C.parameters,
+                 species=V.species,
+                 reaction_system=set_model,
+                 pval=param_values,
+                 ival=initial_values,
+                 obs=observables,
+                 sim=NumericalSimulation(),
+                 exp=ExperimentalData(),
+                 sp=SearchParam(),
+                 obj_func=objective,
+                 rxn=ReactionNetwork()):
+        self.parameters = parameters
+        self.species = species
+        self.reaction_system = reaction_system
+        self.pval = pval
+        self.ival = ival
+        self.obs = obs
+        self.obj_func = obj_func
+        self.sim = sim
+        self.exp = exp
+        self.sp = sp
+        self.rxn = rxn
 
-def get_model_properties():
-    """ Get number of model reactions, species and parameters
-    """
-    rxn = ReactionNetwork()
-    biological_processes = rxn.group()
-    model_reactions = 0
-    for reactions_in_process in biological_processes:
-        model_reactions += len(reactions_in_process)
-    model_species = V.n_species
-    model_parameters = C.n_parameters
+    def get_properties(self):
+        """ Get number of model reactions, species and parameters
+        """
+        biological_processes = self.rxn.group()
+        model_reactions = 0
+        for reactions_in_process in biological_processes:
+            model_reactions += len(reactions_in_process)
+        model_species = len(self.species)
+        model_parameters = len(self.parameters)
 
-    print(
-        "{:d} reactions\n{:d} species\n{:d} parameters".format(
-            model_reactions, model_species, model_parameters
+        print(
+            "{:d} reactions\n{:d} species\n{:d} parameters".format(
+                model_reactions, model_species, model_parameters
+            )
         )
-    )
 
 
-def get_optimization_results():
-    """ Get optimized parameters as CSV file format
+    def get_optimization_results(self):
+        """ Get optimized parameters as CSV file format
 
-    Output
-    ------
-    optimized_params.csv
-    optimized_initial_values.csv
-    """
-    n_file = []
-    fitparam_files = os.listdir('./out')
-    for file in fitparam_files:
-        if re.match(r'\d', file):
-            n_file.append(int(file))
+        Output
+        ------
+        optimized_params.csv
+        optimized_initial_values.csv
+        """
+        n_file = []
+        fitparam_files = os.listdir('./out')
+        for file in fitparam_files:
+            if re.match(r'\d', file):
+                n_file.append(int(file))
+        empty_folder = []
+        for i, nth_paramset in enumerate(n_file):
+            if not os.path.isfile('./out/{:d}/generation.npy'.format(nth_paramset)):
+                empty_folder.append(i)
+    
+        for i in sorted(empty_folder, reverse=True):
+            n_file.pop(i)
 
-    search_idx = get_search_index()
+        if len(self.sp.idx_params) > 0:
+            optimized_params = np.empty(
+                (len(self.sp.idx_params)+2, len(n_file)+1), dtype='<U21'
+            )
+            for i, param_index in enumerate(self.sp.idx_params):
+                for j, nth_paramset in enumerate(n_file):
+                    best_generation = np.load(
+                        './out/{:d}/generation.npy'.format(
+                            nth_paramset
+                        )
+                    )
+                    best_indiv = np.load(
+                        './out/{:d}/fit_param{:d}.npy'.format(
+                            nth_paramset, int(best_generation)
+                        )
+                    )
+                    error = np.load(
+                        './out/{:d}/best_fitness.npy'.format(
+                            nth_paramset
+                        )
+                    )
+                    optimized_params[0, 0] = ''
+                    optimized_params[1, 0] = '*Error*'
+                    optimized_params[i+2, 0] = self.parameters[param_index]
+                    optimized_params[0, nth_paramset-1] = str(nth_paramset)
+                    optimized_params[1, nth_paramset-1] = '{:8.3e}'.format(error)
+                    optimized_params[i+2, nth_paramset-1] = '{:8.3e}'.format(best_indiv[i])
+            with open('optimized_params.csv', 'w') as f:
+                writer = csv.writer(f, lineterminator='\n')
+                writer.writerows(optimized_params)
 
-    if len(search_idx[0]) > 0:
-        optimized_params = np.empty(
-            (len(search_idx[0])+2, len(n_file)+1), dtype='<U21'
-        )
-        for i, param_index in enumerate(search_idx[0]):
-            for j, nth_paramset in enumerate(n_file):
-                best_generation = np.load(
-                    './out/{:d}/generation.npy'.format(
-                        nth_paramset
+        if len(self.sp.idx_initials) > 0:
+            optimized_initials = np.empty(
+                (len(self.sp.idx_initials)+2, len(n_file)+1), dtype='<U21'
+            )
+            for i, specie_index in enumerate(self.sp.idx_initials):
+                for j, nth_paramset in enumerate(n_file):
+                    best_generation = np.load(
+                        './out/{:d}/generation.npy'.format(
+                            nth_paramset
+                        )
                     )
-                )
-                best_indiv = np.load(
-                    './out/{:d}/fit_param{:d}.npy'.format(
-                        nth_paramset, int(best_generation)
+                    best_indiv = np.load(
+                        './out/{:d}/fit_param{:d}.npy'.format(
+                            nth_paramset, int(best_generation)
+                        )
                     )
-                )
-                error = np.load(
-                    './out/{:d}/best_fitness.npy'.format(
-                        nth_paramset
+                    error = np.load(
+                        './out/{:d}/best_fitness.npy'.format(
+                            nth_paramset
+                        )
                     )
-                )
-                optimized_params[0, 0] = ''
-                optimized_params[1, 0] = '*Error*'
-                optimized_params[i+2, 0] = C.param_names[param_index]
-                optimized_params[0, nth_paramset] = str(nth_paramset)
-                optimized_params[1, nth_paramset] = '{:8.3e}'.format(error)
-                optimized_params[i+2, nth_paramset] = '{:8.3e}'.format(best_indiv[i])
-        with open('optimized_params.csv', 'w') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerows(optimized_params)
-
-    if len(search_idx[1]) > 0:
-        optimized_initials = np.empty(
-            (len(search_idx[1])+2, len(n_file)+1), dtype='<U21'
-        )
-        for i, specie_index in enumerate(search_idx[1]):
-            for j, nth_paramset in enumerate(n_file):
-                best_generation = np.load(
-                    './out/{:d}/generation.npy'.format(
-                        nth_paramset
-                    )
-                )
-                best_indiv = np.load(
-                    './out/{:d}/fit_param{:d}.npy'.format(
-                        nth_paramset, int(best_generation)
-                    )
-                )
-                error = np.load(
-                    './out/{:d}/best_fitness.npy'.format(
-                        nth_paramset
-                    )
-                )
-                optimized_initials[0, 0] = ''
-                optimized_initials[1, 0] = '*Error*'
-                optimized_initials[i+2, 0] = V.species[specie_index]
-                optimized_initials[0, nth_paramset] = str(nth_paramset)
-                optimized_initials[1, nth_paramset] = '{:8.3e}'.format(error)
-                optimized_initials[i+2, nth_paramset] = \
-                    '{:8.3e}'.format(best_indiv[i+len(search_idx[0])])
-        with open('optimized_inital_varlues.csv', 'w') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerows(optimized_initials)
+                    optimized_initials[0, 0] = ''
+                    optimized_initials[1, 0] = '*Error*'
+                    optimized_initials[i+2, 0] = self.species[specie_index]
+                    optimized_initials[0, nth_paramset-1] = str(nth_paramset)
+                    optimized_initials[1, nth_paramset-1] = '{:8.3e}'.format(error)
+                    optimized_initials[i+2, nth_paramset-1] = \
+                        '{:8.3e}'.format(best_indiv[i+len(self.sp.idx_params)])
+            with open('optimized_inital_varlues.csv', 'w') as f:
+                writer = csv.writer(f, lineterminator='\n')
+                writer.writerows(optimized_initials)
