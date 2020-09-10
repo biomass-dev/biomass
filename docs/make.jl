934d096def75454223d82c6dd69517f4b2e33cd4
himoto <31299606+himoto@users.noreply.github.com>
2020-09-10 15:45:03 +0900

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,7 @@ makedocs(;
     pages = [
         "Home" => "index.md",
         "Getting started with BioMASS" => [
+            "Installation" => "introduction/set_up.md",
             "Import model" => "introduction/import_model.md",
             "Parameter estimation" => "introduction/parameter_estimation.md",
             "Visualization of simulation results" => "introduction/visualization.md",
