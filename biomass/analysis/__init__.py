0592d39a8c7d0873f00fe8cc2ae2e61ca9b53937
himoto <31299606+himoto@users.noreply.github.com>
2020-11-04 12:46:41 +0900

    Don't use wildcard import in analysis

--- biomass/analysis/__init__.py
+++ biomass/analysis/__init__.py
@@ -1,4 +1,4 @@
-from .signaling_metric import *
+from .signaling_metric import get_signaling_metric, dlnyi_dlnxj
 from .reaction import ReactionSensitivity
 from .initial_condition import InitialConditionSensitivity
 from .parameter import ParameterSensitivity
