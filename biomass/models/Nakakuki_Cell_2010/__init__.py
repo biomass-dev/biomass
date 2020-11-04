296d0814a86b75aad59bb13482a8a334ddf3b85f
himoto <31299606+himoto@users.noreply.github.com>
2020-11-04 12:22:29 +0900

    Don't use wildcard imports

--- biomass/models/Nakakuki_Cell_2010/__init__.py
+++ biomass/models/Nakakuki_Cell_2010/__init__.py
@@ -1,6 +1,6 @@
 from .name2idx import C, V
-from .set_model import *
-from .observable import *
+from .set_model import DifferentialEquation, param_values, initial_values
+from .observable import observables, NumericalSimulation, ExperimentalData
 from .viz import Visualization
 from .fitness import objective
 from .set_search_param import SearchParam
