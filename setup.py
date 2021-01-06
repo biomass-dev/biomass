154febb85a6737716a1ad51441ae5919c4c06fc0
himoto <31299606+himoto@users.noreply.github.com>
2021-01-06 09:46:15 +0900

    Update classifiers

--- setup.py
+++ setup.py
@@ -39,9 +39,12 @@ def main():
             "Intended Audience :: Science/Research",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Scientific/Engineering",
             "Topic :: Scientific/Engineering :: Bio-Informatics",
         ],
     )
