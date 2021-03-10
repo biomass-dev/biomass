67452185efeaf5219b1b5bf80713226005e69792
himoto <31299606+himoto@users.noreply.github.com>
2021-03-10 18:29:07 +0900

    Update tests

---
+++ tests/test_insulin_signaling.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+import numpy as np
+
+from biomass import ModelObject, run_simulation
+from biomass.models import insulin_signaling
+
+model = ModelObject(insulin_signaling.create())
+
+for dir in ["figure", "simulation_data"]:
+    if os.path.isdir(os.path.join(model.path, dir)):
+        shutil.rmtree(os.path.join(model.path, dir))
+
+
+def test_simulate_successful():
+    x = model.pval()
+    y0 = model.ival()
+    assert model.sim.simulate(x, y0) is None
+
+
+def test_run_simulation():
+    run_simulation(model, viz_type="original")
+    simulated_value = np.load(
+        os.path.join(
+            model.path,
+            "simulation_data",
+            "simulations_original.npy",
+        )
+    )
+    assert np.isfinite(simulated_value).all()
+
+
+def test_cleanup():
+    for dir in ["figure", "simulation_data"]:
+        if os.path.isdir(os.path.join(model.path, dir)):
+            shutil.rmtree(os.path.join(model.path, dir))
