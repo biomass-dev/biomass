102480548aa95b965d7b5047a718e1ffc37743f4
himoto <31299606+himoto@users.noreply.github.com>
2021-07-05 11:38:21 +0900

    Update tests

--- tests/test_mapk_cascade.py
+++ tests/test_mapk_cascade.py
@@ -122,7 +122,7 @@ def test_run_simulation():
     for viz_type in ["original", "1", "2", "3", "average", "best"]:
         for obs_name in model.observables:
             assert os.path.isfile(
-                os.path.join(model.path, "figure", "simulation", f"{viz_type}", f"{obs_name}.pdf")
+                os.path.join(model.path, "figure", "simulation", f"{viz_type}", f"{obs_name}.png")
             )
 
 
@@ -162,22 +162,61 @@ def test_save_result():
     )
 
 
+def _get_duration(
+    time_course: np.ndarray,
+    below_threshold: float = 0.5,
+) -> int:
+    """
+    Calculation of the duration as the time it takes to decline below the threshold.
+
+    Parameters
+    ----------
+    timecourse : array
+        Simulated time course data.
+
+    below_threshold : float (from 0.0 to 1.0)
+        0.5 for 50% of its maximum.
+
+    Returns
+    -------
+    duration : int
+
+    """
+    if not 0.0 < below_threshold < 1.0:
+        raise ValueError("below_threshold must lie within (0.0, 1.0).")
+    maximum_value = np.max(time_course)
+    t_max = np.argmax(time_course)
+    time_course = time_course - below_threshold * maximum_value
+    time_course[time_course > 0.0] = -np.inf
+    duration = np.argmax(time_course[t_max:]) + t_max
+
+    return duration
+
+
 def test_sensitivity_analysis():
     for target in [
-        "parameter",
+        # "parameter",
         "initial_condition",
-        "reaction",
+        # "reaction",
     ]:
         for metric in [
             "maximum",
             "minimum",
+            "integral",
             "argmax",
-            "argmin",
             "timepoint",
             "duration",
-            "integral",
         ]:
-            run_analysis(model, target=target, metric=metric)
+            run_analysis(
+                model,
+                target=target,
+                metric=metric,
+                create_metrics={
+                    "argmax": np.argmax,
+                    "timepoint": lambda time_course: time_course[model.problem.t[-1]],
+                    "duration": _get_duration,
+                },
+            )
             sensitivity_coefficients = np.load(
                 os.path.join(
                     model.path,
