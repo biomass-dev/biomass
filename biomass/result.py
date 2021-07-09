2f532a3eeabbc2697d6f6729ee04af9e135cfa71
himoto <31299606+himoto@users.noreply.github.com>
2021-07-09 16:32:03 +0900

    Recover default rcParams before plt.close()

--- biomass/result.py
+++ biomass/result.py
@@ -187,6 +187,7 @@ class OptimizationResults(ExecModel):
                 "estimated_parameter_sets",
             ),
         )
+        plt.rcParams.update(dict(plt.rcParamsDefault))
         plt.close()
 
     def dynamic_assessment(self, include_original: bool = False) -> None:
