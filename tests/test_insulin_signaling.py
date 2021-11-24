16438a5eccd60d309c95e77bfabeff6a1dc0ce0f
himoto <31299606+himoto@users.noreply.github.com>
2021-11-24 12:03:43 +0000

    Add visualization functions used in examples to tests

--- tests/test_insulin_signaling.py
+++ tests/test_insulin_signaling.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from biomass import Model, run_simulation
@@ -19,16 +20,34 @@ def test_simulate_successful():
     assert model.problem.simulate(x, y0) is None
 
 
-def test_run_simulation():
-    run_simulation(model)
-    simulated_value = np.load(
-        os.path.join(
-            model.path,
-            "simulation_data",
-            "simulations_original.npy",
-        )
-    )
-    assert np.isfinite(simulated_value).all()
+def test_example_plot():
+    assert run_simulation(model) is None
+    res = np.load(os.path.join(model.path, "simulation_data", "simulations_original.npy"))
+
+    plt.figure(figsize=(8, 8))
+    plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+    plt.rcParams['font.size'] = 18
+    plt.rcParams['lines.linewidth'] = 4
+    plt.rcParams['lines.markersize'] = 16
+
+    plt.subplots_adjust(wspace=0.25, hspace=0.3)
+
+    for i, obs_name in enumerate(model.observables):
+        plt.subplot(2, 2, i + 1)
+        for j, color in enumerate(['darkblue', 'cornflowerblue', 'yellowgreen', 'goldenrod', 'brown']):
+            plt.plot(model.problem.t, res[i, :, j], color=color)
+        plt.xlim(0, 480)
+        plt.xticks([0, 120, 240, 360, 480], fontweight='bold')
+        plt.ylim(0, 1)
+        plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], fontweight='bold')
+        plt.title(f'{obs_name}', fontweight='bold')
+        if i % 2 == 0:
+            plt.ylabel('Intensity (A.U.)', fontweight='bold')
+        if i > 1:
+            plt.xlabel('                           time (min)', fontweight='bold')
+    plt.show()
 
 
 def test_cleanup():
--- tests/test_tgfb_smad.py
+++ tests/test_tgfb_smad.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from biomass import Model, run_simulation
@@ -21,15 +22,49 @@ def test_simulate_successful():
 
 
 def test_run_simulation():
-    run_simulation(model)
-    simulated_value = np.load(
-        os.path.join(
-            model.path,
-            "simulation_data",
-            "simulations_original.npy",
-        )
-    )
-    assert np.isfinite(simulated_value).all()
+    assert run_simulation(model) is None
+    res = np.load(os.path.join(model.path, "simulation_data", "simulations_original.npy"))
+
+    plt.figure(figsize=(10, 10))
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams['mathtext.fontset'] = 'custom'
+    plt.rcParams['mathtext.it'] = 'Arial:italic'
+    plt.rcParams['axes.linewidth'] = 2
+    plt.rcParams['xtick.major.width'] = 2
+    plt.rcParams['ytick.major.width'] = 2
+    plt.rcParams['lines.linewidth'] = 2
+    plt.subplots_adjust(wspace=0.3, hspace=0.8)
+
+    yticks = {
+        'Ski': [0, 1, 2],
+        'Skil': [0, 1, 2, 3],
+        'Dnmt3a': [0, 0.5, 1, 1.5],
+        'Sox4': [0, 1, 2],
+        'Jun': [0, 1, 2, 3],
+        'Smad7': [0, 1, 2, 3],
+        'Klf10': [0, 2, 4],
+        'Bmp4': [0, 1, 2],
+        'Cxcl15': [-1.5, -1, -0.5, 0, 0.5],
+        'Dusp5': [-1.5, -1, -0.5, 0],
+        'Tgfa': [-1.5, -1, -0.5, 0, 0.5],
+        'Pdk4': [-3, -2, -1, 0],
+    }
+
+    for i, gene_name in enumerate(model.observables):
+        plt.subplot(4, 3, i+1)
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['top'].set_visible(False)
+        for j, color in enumerate(['brown', 'royalblue', 'darkgreen', 'k']):
+            plt.plot(model.problem.t, res[i, :, j], color=color, linewidth=1.5)
+            plt.title(fr'$\it{gene_name}$')
+            plt.yticks(yticks[gene_name])
+        if i >= 9:
+            plt.xlabel('time (min)')
+        plt.xticks([0, 200, 400, 600])
+        if i % 3 == 0:
+            plt.ylabel('gene expression(log2)')
+    plt.show()
 
 
 def test_cleanup():
