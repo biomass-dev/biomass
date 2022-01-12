b9c87861be640b9d64e393867632e3e8b1b07ee5
Hiroaki Imoto <himoto@protein.osaka-u.ac.jp>
2022-01-12 22:01:03 +0000

    Move flux vector to `reaction_network.py` (#85)
    * Move flux vector to reaction_network.py
    
    * Move flux vector to reaction_network.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- biomass/models/insulin_signaling/set_model.py
+++ biomass/models/insulin_signaling/set_model.py
@@ -1,45 +1,19 @@
 from .name2idx import C, V
+from .reaction_network import ReactionNetwork
 
 
-class DifferentialEquation(object):
+class DifferentialEquation(ReactionNetwork):
     def __init__(self, perturbation):
         super(DifferentialEquation, self).__init__()
         self.perturbation = perturbation
 
     def diffeq(self, t, y, *x):
 
-        v = {}
+        v = self.flux(t, y, x)
 
-        v[1] = x[C.k1_synthesis] * (y[V.pro_IRcom] - y[V.IRcom])
-        v[2] = x[C.k1_InsIRcom] * y[V.Ins] * y[V.IRcom] - x[C.k2_InsIRcom] * y[V.p1IRcom]
-        v[3] = x[C.k1_p1IRcomDeg] * y[V.p1IRcom]
-        v[4] = x[C.k1_p1IRcomPhos] * y[V.pmTOR] * y[V.p1IRcom]
-        v[5] = x[C.k1_p1p2IRcomdePhos] * y[V.p1p2IRcom]
-        v[6] = x[C.k1_IRcomPhos] * y[V.pmTOR] * y[V.IRcom]
-        v[7] = x[C.k1_p2IRcomdePhos] * y[V.p2IRcom]
-        v[8] = x[C.k1_p2IRcomDeg] * y[V.p2IRcom]
-        v[9] = x[C.k1_Insp2IRcom] * y[V.Ins] * y[V.p2IRcom] - x[C.k2_Insp2IRcom] * y[V.p1p2IRcom]
-        v[10] = x[C.k1_p1p2IRcomDeg] * y[V.p1p2IRcom]
-        v[11] = x[C.k1_AKTPhos] * (y[V.iAKT] - y[V.pAKT]) * y[V.p1IRcom]
-        v[12] = x[C.k1_pAKTdePhos] * y[V.pAKT]
-        v[13] = x[C.k1_mTORPhos] * (y[V.imTOR] - y[V.pmTOR]) * y[V.pAKT]
-        v[14] = x[C.k1_pmTORdePhos] * y[V.pmTOR]
-        v[15] = x[C.k1_S6KPhos] * (y[V.iS6K] - y[V.pS6K]) * y[V.pmTOR]
-        v[16] = x[C.k1_pS6KdePhos] * y[V.pS6K] * y[V.pX]
-        v[17] = x[C.k1_XPhos] * (y[V.iX] - y[V.pX]) * y[V.pmTOR]
-        v[18] = x[C.k1_pXdePhos] * y[V.pX]
-        v[19] = x[C.k1_GSK3BPhos] * (y[V.iGSK3B] - y[V.pGSK3B]) * y[V.pAKT]
-        v[20] = x[C.k1_pGSK3BdePhos] * y[V.pGSK3B]
-        v[21] = x[C.k1_FoxO1Phos] * (y[V.iFoxO1] - y[V.pFoxO1]) * y[V.pAKT]
-        v[22] = x[C.k1_pFoxO1dePhos] * y[V.pFoxO1]
-        v[23] = x[C.k1_G6PaseSynthesis] * (y[V.iFoxO1] - y[V.pFoxO1])
-        v[24] = x[C.k1_G6PaseDeg] * y[V.G6Pase]
-
-        """
         if self.perturbation:
             for i, dv in self.perturbation.items():
                 v[i] = v[i] * dv
-        """
 
         dydt = [0] * V.NUM
 
