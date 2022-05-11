6db6826c7a2a59d6e3bb56ff0f04575a633c6f96
himoto <31299606+himoto@users.noreply.github.com>
2022-05-11 16:21:42 +0900

    Update external_optimizer

--- biomass/estimation/__init__.py
+++ biomass/estimation/__init__.py
@@ -1,3 +1,2 @@
 from .external_optimizer import ExternalOptimizer
-from .de import ScipyDifferentialEvolution
 from .search_util import convert_scale, initialize_search_param
--- biomass/estimation/external_optimizer.py
+++ biomass/estimation/external_optimizer.py
@@ -10,21 +10,26 @@ import numpy as np
 from ..exec_model import ExecModel, ModelObject
 
 
+DIRNAME = "_tmp"
+
+
 class _Logger(object):
     """
-    Duplicate stdout to _external/optimization.log.
+    Duplicate stdout to _tmp/optimization.log.
     """
 
-    def __init__(self, model_path: str):
+    def __init__(self, model_path: str, disp_here: bool):
+        self.disp_here = disp_here
         self.terminal = sys.stdout
         self.log = open(
-            os.path.join(model_path, "out", "_external", "optimization.log"),
+            os.path.join(model_path, "out", DIRNAME, "optimization.log"),
             mode="w",
             encoding="utf-8",
         )
 
     def write(self, message: str):
-        self.terminal.write(message)
+        if self.disp_here:
+            self.terminal.write(message)
         self.log.write(message)
 
 
@@ -35,34 +40,81 @@ class ExternalOptimizer(ExecModel):
 
     Attributes
     ----------
-
     model : ModelObject
         The BioMASS model object.
-
     optimize : Callable
-        The external optimizer, e.g., ``scipy.optimize.differential_evolution``.
+        The external optimizer, e.g., :func:`scipy.optimize.differential_evolution`.
+
+    Examples
+    --------
+    >>> from scipy.optimize import differential_evolution
+    >>> from biomass import Model
+    >>> from biomass.estimation import ExternalOptimizer
+    >>> from biomass.models import Nakakuki_Cell_2010
+    >>> model = Model(Nakakuki_Cell_2010.__package__).create()
+    >>> optimizer = ExternalOptimizer(model, differential_evolution)
+    >>> def obj_fun(x):
+    ...    '''Objective function to be minimized.'''
+    ...    return optimizer.get_obj_val(x)
+    >>> res = optimizer.minimize(
+    ...     obj_fun,
+    ...     [(0, 1) for _ in range(len(model.problem.bounds))],
+    ...     strategy="best1bin",
+    ...     maxiter=50,
+    ...     tol=1e-4,
+    ...     mutation=0.1,
+    ...     recombination=0.5,
+    ...     disp=True,
+    ...     polish=False,
+    ...     workers=-1,
+    ... )
+
+    differential_evolution step 1: f(x)= 5.19392\n
+    differential_evolution step 2: f(x)= 2.32477\n
+    differential_evolution step 3: f(x)= 1.93583\n
+    ...\n
+    differential_evolution step 50: f(x)= 0.519774\n
+
+    >>> from biomass import run_simulation
+    >>> param_values = model.problem.gene2val(res.x)
+    >>> optimizer.import_solution(param_values, x_id=0)
+    >>> run_simulation(model, viz_type="0")
     """
 
     model: ModelObject
     optimize: Callable
+    disp_here: bool = False
 
     def __post_init__(self):
-        os.makedirs(os.path.join(self.model.path, "out", "_external"), exist_ok=True)
+        os.makedirs(os.path.join(self.model.path, "out", DIRNAME), exist_ok=True)
         self.default_stdout = sys.stdout
 
-    def run(self, *args, **kwargs):
+    def minimize(self, *args, **kwargs):
         """
         Execute the external optimizer.
         """
-        os.makedirs(os.path.join(self.model.path, "out", "_external"), exist_ok=True)
+        os.makedirs(os.path.join(self.model.path, "out", DIRNAME), exist_ok=True)
         try:
-            sys.stdout = _Logger(self.model.path)
+            sys.stdout = _Logger(self.model.path, self.disp_here)
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 res = self.optimize(*args, **kwargs)
+            return res
         finally:
             sys.stdout = self.default_stdout
-        return res
+
+    def _get_n_iter(self, savedir: str) -> int:
+        n_iter: int = 0
+        with open(
+            os.path.join(savedir, "optimization.log"),
+            mode="r",
+            encoding="utf-8",
+        ) as f:
+            log_file = f.readlines()
+        for message in log_file:
+            if len(message.strip()) > 0:
+                n_iter += 1
+        return n_iter
 
     def import_solution(self, x: Union[np.ndarray, List[float]], x_id: int = 0) -> None:
         """
@@ -76,79 +128,20 @@ class ExternalOptimizer(ExecModel):
             The solution of the optimization.
         x_id : int (default: 0)
             Index of the parameter set.
-
-        Examples
-        --------
-        >>> from scipy.optimize import differential_evolution
-        >>> from biomass import Model
-        >>> from biomass.estimation import ExternalOptimizer
-        >>> from biomass.models import Nakakuki_Cell_2010
-        >>> model = Model(Nakakuki_Cell_2010.__package__).create()
-        >>> optimizer = ExternalOptimizer(model, differential_evolution)
-        >>> def obj_fun(x):
-        ...    '''Objective function to be minimized.'''
-        ...    return optimizer.get_obj_val(x)
-        >>> res = optimizer.run(
-        ...     obj_fun,
-        ...     [(0, 1) for _ in range(len(model.problem.bounds))],
-        ...     strategy="best1bin",
-        ...     maxiter=50,
-        ...     tol=1e-4,
-        ...     mutation=0.1,
-        ...     recombination=0.5,
-        ...     disp=True,
-        ...     polish=False,
-        ...     workers=-1,
-        ... )
-
-        differential_evolution step 1: f(x)= 5.19392\n
-        differential_evolution step 2: f(x)= 2.32477\n
-        differential_evolution step 3: f(x)= 1.93583\n
-        ...\n
-        differential_evolution step 50: f(x)= 0.519774\n
-
-        >>> from biomass import run_simulation
-        >>> param_values = model.problem.gene2val(res.x)
-        >>> optimizer.import_solution(param_values, x_id=0)
-        >>> run_simulation(model, viz_type="0")
         """
-
-        if os.path.isdir(os.path.join(self.model.path, "out", f"{x_id:d}")):
+        savedir = os.path.join(self.model.path, "out", f"{x_id:d}")
+        if os.path.isdir(savedir):
             raise ValueError(
                 f"out{os.sep}{x_id:d} already exists in {self.model.path}. "
                 "Use another parameter id."
             )
         else:
-            os.makedirs(os.path.join(self.model.path, "out", f"{x_id:d}"))
-        shutil.move(
-            os.path.join(self.model.path, "out", "_external", "optimization.log"),
-            os.path.join(self.model.path, "out", f"{x_id:d}"),
-        )
+            os.makedirs(savedir)
+        shutil.move(os.path.join(self.model.path, "out", DIRNAME, "optimization.log"), savedir)
 
         best_fitness: float = self.model.problem.objective(x)
-        n_iter: int = 0
-        with open(
-            os.path.join(self.model.path, "out", f"{x_id:d}", "optimization.log"),
-            mode="r",
-            encoding="utf-8",
-        ) as f:
-            log_file = f.readlines()
-        for message in log_file:
-            if len(message.strip()) > 0:
-                n_iter += 1
-        np.save(
-            os.path.join(self.model.path, "out", f"{x_id:d}", "best_fitness"),
-            best_fitness,
-        )
-        np.save(
-            os.path.join(self.model.path, "out", f"{x_id:d}", "count_num"),
-            n_iter,
-        )
-        np.save(
-            os.path.join(self.model.path, "out", f"{x_id:d}", "generation"),
-            n_iter,
-        )
-        np.save(
-            os.path.join(self.model.path, "out", f"{x_id:d}", f"fit_param{n_iter:d}"),
-            x,
-        )
+        n_iter = self._get_n_iter(savedir)
+        np.save(os.path.join(savedir, "best_fitness"), best_fitness)
+        np.save(os.path.join(savedir, "count_num"), n_iter)
+        np.save(os.path.join(savedir, "generation"), n_iter)
+        np.save(os.path.join(savedir, f"fit_param{n_iter:d}"), x)
