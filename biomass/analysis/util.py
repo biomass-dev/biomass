534ef5c174a4acd972a36ecff32139293959ae0f
himoto <31299606+himoto@users.noreply.github.com>
2022-08-26 12:29:32 +0100

    Update code for sensitivity analysis

--- biomass/analysis/util.py
+++ biomass/analysis/util.py
@@ -1,17 +1,19 @@
-import sys
 from dataclasses import dataclass, field
-from math import fabs, isnan, log
-from typing import Callable, Dict, List, Union
+from math import isnan, log
+from typing import Callable, Final, Dict, Union
 
 import numpy as np
+from numba import njit
 from scipy.integrate import simpson
+
+
+EPS: Final[np.float64] = np.finfo(float).eps
 
 
 @dataclass
 class SignalingMetric(object):
     """
     Signaling metric used for sensitivity analysis.
-
     Attributes
     ----------
     quantification : Dict[str, Callable[[np.ndarray], Union[int, float]]]
@@ -28,56 +30,48 @@ class SignalingMetric(object):
     )
 
 
+@njit(fastmath=True)
 def dlnyi_dlnxj(
     signaling_metric: np.ndarray,
-    n_file: List[int],
-    perturbed_idx: List[int],
-    observables: List[str],
-    conditions: List[str],
+    num_file_ids: int,
+    num_perturbed_ids: int,
+    num_observables: int,
+    num_conditions: int,
     rate: float,
 ) -> np.ndarray:
     """
     Numerical computation of sensitivities using finite difference approximations
     with 1% changes in the reaction rates, parameter values or non-zero initial values.
-
     Parameters
     ----------
     signaling_metric : numpy array
         Signaling metric
-
-    n_file : list of integers
-        Optimized parameter sets in out/
-
-    perturbed_idx : list of integers
-        Indices of rate equations or non-zero initial values.
-
-    observables : list of strings
-        observables in observable.py
-
-    conditions : list of strings
-        Experimental conditions.
-
+    num_file_ids : int
+        Number of ptimized parameter sets in out/
+    num_perturbed_ids : int
+        Number of parameters, rate equations, or non-zero initial conditions to be perturbed.
+    num_observables : int
+        Number of observables in observable.py
+    num_conditions : int
+        Number of experimental conditions.
     rate : float ~ 1
         1.01 for 1% change.
-
     Returns
     -------
     sensitivity_coefficients: numpy array
-
     """
     sensitivity_coefficients = np.empty(
-        (len(n_file), len(perturbed_idx), len(observables), len(conditions))
+        (num_file_ids, num_perturbed_ids, num_observables, num_conditions),
     )
-    for i, _ in enumerate(n_file):
-        for j, _ in enumerate(perturbed_idx):
-            for k, _ in enumerate(observables):
-                for l, _ in enumerate(conditions):
+    for i in range(num_file_ids):
+        for j in range(num_perturbed_ids):
+            for k in range(num_observables):
+                for l in range(num_conditions):
                     if isnan(signaling_metric[i, j, k, l]):
                         sensitivity_coefficients[i, j, k, l] = np.nan
                     elif (
-                        fabs(signaling_metric[i, -1, k, l]) < sys.float_info.epsilon
-                        or fabs(signaling_metric[i, j, k, l] - signaling_metric[i, -1, k, l])
-                        < sys.float_info.epsilon
+                        abs(signaling_metric[i, -1, k, l]) < EPS
+                        or abs(signaling_metric[i, j, k, l] - signaling_metric[i, -1, k, l]) < EPS
                         or (signaling_metric[i, j, k, l] / signaling_metric[i, -1, k, l]) <= 0
                     ):
                         # 1. Signaling metric before adding perturbation is zero
@@ -90,3 +84,23 @@ def dlnyi_dlnxj(
                         ) / log(rate)
 
     return sensitivity_coefficients
+
+
+@njit(fastmath=True)
+def remove_nan(sensitivity_matrix: np.ndarray) -> np.ndarray:
+    """
+    Remove NaN from sensitivity matrix. This function is used for preprocessing of visualizing
+    the result of sensitivity analysis through a heatmap.
+    Parameters
+    ----------
+    sensitivity_matrix : numpy.ndarray
+        M x N matrix, where M and M are # of parameter sets and # of perturbed objects, respectively.
+    """
+    nan_idx = []
+    for i in range(sensitivity_matrix.shape[0]):
+        if np.isnan(sensitivity_matrix[i, :]).any():
+            nan_idx.append(i)
+        if np.nanmax(np.abs(sensitivity_matrix[i, :])) < EPS:
+            sensitivity_matrix[i, :] = np.zeros(sensitivity_matrix.shape[1])
+
+    return np.delete(sensitivity_matrix, nan_idx, axis=0)
