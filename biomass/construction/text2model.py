78369fd7099634338e245f5b932ed114da935087
himoto <31299606+himoto@users.noreply.github.com>
2022-09-27 11:09:03 +0100

    Move description to docs

--- biomass/construction/text2model.py
+++ biomass/construction/text2model.py
@@ -1,17 +1,3 @@
-"""
-The Text2Model class converts texts and sentences on biochemical systems into an executable
-mathematical model. It was originally developed in the pasmopy project [1]_, [2]_.
-
-References
-----------
-.. [1] Imoto, H., Yamashiro, S. & Okada, M. A text-based computational framework for patient
-        -specific modeling for classification of cancers. iScience 25, 103944 (2022).
-.. [2] Imoto, H., Yamashiro, S., Murakami, K. & Okada, M. Protocol for stratification of
-        triple-negative breast cancer patients using in silico signaling dynamics.
-        STAR Protocols 3, 101619 (2022).
-"""
-
-
 import os
 import re
 import shutil
